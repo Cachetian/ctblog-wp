04eeed83c48911ca7dfaa5a5dff86542678d17b9
Sky <1989mutian@163.com>
2020-02-26 23:04:30 +0800

    Upgrade to 5.3.2

--- wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -10,7 +10,15 @@
 
 ?>
 <div class="site-info">
-        <a href="http://www.miitbeian.gov.cn/" rel="external nofollow" target="_blank"><?php echo get_option( 'zh_cn_l10n_icp_num' );?></a>
-        <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
-        <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000366812'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1000366812%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
-</div><!-- .site-info-->
+	<?php
+	if ( function_exists( 'the_privacy_policy_link' ) ) {
+		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+	}
+	?>
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>" class="imprint">
+		<?php
+			/* translators: %s: WordPress */
+		printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' );
+		?>
+	</a>
+</div><!-- .site-info -->
